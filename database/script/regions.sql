d94b31f431a33f87a00fb874e4f45a0696c0f05d
rigoCaicedo <rigoberto.caicedo@soluciones.software>
2019-09-11 16:30:21 -0500

    script añadido

---
+++ database/script/regions.sql
@@ -0,0 +1,37 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-09-2019 a las 23:28:58
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `clientes_gestion`
+--
+
+--
+-- Volcado de datos para la tabla `regions`
+--
+
+INSERT INTO `regions` (`id`, `nombreReg`, `created_at`, `updated_at`) VALUES
+(1, 'Caribe', NULL, NULL),
+(2, 'Andina', NULL, NULL),
+(3, 'Orinoquia', NULL, NULL);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
